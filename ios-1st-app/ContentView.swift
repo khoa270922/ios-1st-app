57266badeace12226e9c30ea542e6945e45a9285
khoa <khoa270922@gmail.com>
2025-07-24 20:59:40 +0700

    Initial Commit

---
+++ ios-1st-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-1st-app
+//
+//  Created by kenny jade on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-1st-app/ios_1st_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_1st_appApp.swift
+//  ios-1st-app
+//
+//  Created by kenny jade on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_1st_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
